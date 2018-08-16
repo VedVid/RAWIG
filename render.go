e18c5de5a060771f9d814f9bb8b36e172fa9ee82
VedVid <toalak@gmail.com>
2018-08-16 10:30:49 +0200

    better use of range

--- render.go
+++ render.go
@@ -10,26 +10,26 @@ const (
 )
 
 func PrintBoard(b Board) {
-	for i := range b {
-		blt.Layer(b[i].Block.Layer)
-		glyph := "[color=" + b[i].Block.Color + "]" + b[i].Block.Char
-		blt.Print(b[i].Block.X, b[i].Block.Y, glyph)
+	for i, v := range b {
+		blt.Layer(v.Block.Layer)
+		glyph := "[color=" + v.Block.Color + "]" + v.Block.Char
+		blt.Print(v.Block.X, v.Block.Y, glyph)
 	}
 }
 
 func PrintObjects(o Objects) {
-	for i := range o {
-		blt.Layer(o[i].Block.Layer)
-		glyph := "[color=" + o[i].Block.Color + "]" + o[i].Block.Char
-		blt.Print(o[i].Block.X, o[i].Block.Y, glyph)
+	for i, v := range o {
+		blt.Layer(v.Block.Layer)
+		glyph := "[color=" + v.Block.Color + "]" + v.Block.Char
+		blt.Print(v.Block.X, v.Block.Y, glyph)
 	}
 }
 
 func PrintMonsters(m Monsters) {
-	for i := range m {
-		blt.Layer(m[i].Block.Layer)
-		glyph := "[color=" + m[i].Block.Color + "]" + m[i].Block.Char
-		blt.Print(m[i].Block.X, m[i].Block.Y, glyph)
+	for i, v := range m {
+		blt.Layer(v.Block.Layer)
+		glyph := "[color=" + v.Block.Color + "]" + v.Block.Char
+		blt.Print(v.Block.X, v.Block.Y, glyph)
 	}
 }
 

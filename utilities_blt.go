8c8beee5be238761e156cf1e3026137ef5a192fe
VedVid <toalak@gmail.com>
2018-08-18 01:04:54 +0200

    setting custom tiles using bearlibterminal

---
+++ utilities_blt.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"strconv"
+
+	blt "bearlibterminal"
+)
+
+func SetTile(path, number, filter string, size int) {
+	/*Function SetTile allows to use special tiles (glyphs, bitmaps)
+	as font elements;
+	number variables has to be formatted in that way:
+	U+<unicode-number>, like: U+E001
+	Later, that U+E001 identifier may be used in printing functions, like
+	wall := 0xE001 (note different format!); blt.Print(x, y, wall)*/
+	blt.Set(number + ": " + path + ", resize=" + size + ", resize-filter=" + filter)
+}

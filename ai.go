84472d63eff38ddd9c32d7a4aae1686fcfb03361
VedVid <toalak@gmail.com>
2018-10-22 16:03:50 +0200

    behaviour -> behavior again

--- ai.go
+++ ai.go
@@ -31,10 +31,10 @@ func CreaturesTakeTurn(b Board, c Creatures) {
 	/*Function CreaturesTakeTurn is supposed to handle all enemy creatures
 	actions: movement, attacking, etc.
 	It takes Board and Creatures as arguments.
-	Iterates through all Creatures slice, and handles creature behaviour:
+	Iterates through all Creatures slice, and handles creature behavior:
 	if distance between creature and player is bigger than 1, creature
 	moves towards player. Else, it attacks.
-	It uses switch for matching AIType and behaviour. Skips Creatures with NoAI
+	It uses switch for matching AIType and behavior. Skips Creatures with NoAI
 	(ie corpses) and PlayerAI.
 	At first, I wanted to use map[int]METHOD, but it's not easy to implement.*/
 	for _, v := range c {

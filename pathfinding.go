c1da7238df71bc0e65ddb55b3447c5c22b67fcc7
VedVid <toalak@gmail.com>
2018-11-12 00:53:50 +0100

    not good, but functional

--- pathfinding.go
+++ pathfinding.go
@@ -89,7 +89,7 @@ func (c *Creature) MoveTowardsPath(b Board, tx, ty int) {
 		}
 		frontiers, startFound = FindAdjacent(nodes, frontiers, start, w)
 	}
-	RenderWeights(nodes)
+	//RenderWeights(nodes)
 	dx, dy := BacktrackPath(nodes, start)
 	c.Move(dx, dy, b)
 }

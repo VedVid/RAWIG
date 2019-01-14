57f602321064b5301b3be8c859c5fe30378eac1f
Vedor Vidurakis <toalak@gmail.com>
2019-01-14 01:44:12 +0100

    Added println info while trying to equip to empty

--- objects.go
+++ objects.go
@@ -166,6 +166,7 @@ func HandleEquipping(c *Creature, o *Object, slot int) bool {
 	turnSpent := false
 	if o == nil {
 		//print list of all equippables in inventory, because slot is empty
+		fmt.Println("It should print list of all equippables in inventory, but it is not doing it right now.")
 		return turnSpent
 	} else {
 		//dequip item from slot

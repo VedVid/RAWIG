8ea94782cbee06c68e459d34bd64fdedd721ebd6
Vedor Vidurakis <toalak@gmail.com>
2019-02-19 21:58:02 +0100

    it's truly empty Objects

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ func main() {
 	var actors = new(Creatures)
 	StartGame(cells, actors, objs)
 	for {
+		fmt.Println(objs)
 		RenderAll(*cells, *objs, *actors)
 		key := blt.Read()
 		if key == blt.TK_ESCAPE {
